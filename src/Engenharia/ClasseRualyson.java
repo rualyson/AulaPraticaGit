1ee82e59ad8fe81d5c88ffb55e248ddba79550b5
cleano <cleano@cleano-K45A>
2016-10-17 09:53:22 -0300

    Nova Modificação na aula EG pratica 01

--- src/Engenharia/ClasseRualyson.java
+++ src/Engenharia/ClasseRualyson.java
@@ -1,5 +1,36 @@
 package Engenharia;
 
+import java.util.Scanner;
 public class ClasseRualyson {
-	int a = 2;
+	
+	public static void main(String args[]){
+		
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Informe a quantidade de notas");
+		int quant=ler.nextInt();
+		
+		double totalnotas=0;
+		
+		double []array=new double[quant];
+		
+		for(int i=0 ; i< quant ;i++){
+			double nota=ler.nextDouble();
+			array[i]=nota;
+			totalnotas+=nota;
+			
+		}
+		int cont=1;
+		
+		for(int x=0 ; x< array.length ; x++){
+			System.out.println("Sua  nota "+cont+" foi :"+array[x]);
+			cont++;
+		}
+		
+		System.out.println("Sua média foi: "+ totalnotas/array.length);
+		
+		
+	
+		
+	}
+	
 }
